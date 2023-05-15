3e42c7748eeccf6e17d9c9fcf45bb18f012b7a00
Exudev <pablo.euardo10@gmail.com>
2023-05-14 22:00:34 -0400

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Counter } from "./components/Counter";
+
 function App() {
   return (
     <>
      <h1>Parking Spot</h1>
+     <hr />
+     <Counter/>
     </>
   );
 }
---
+++ src/components/User.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export const User = () => {
+  const [user, setUser] = useState();
+  return (
+    <div>
+      <h3> User: useState</h3>
+
+      <button className="btn">
+
+      </button>
+    </div>
+  )
+}
